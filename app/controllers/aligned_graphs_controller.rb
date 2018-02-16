88d1d8bb855e27b6cc8ab16382f9b13781c1b7d6
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2018-02-16 15:46:36 +0000

    Add lemma/pos/morphology to graphs

--- app/controllers/aligned_graphs_controller.rb
+++ app/controllers/aligned_graphs_controller.rb
@@ -18,6 +18,9 @@ class AlignedGraphsController < ApplicationController
           o.id = t['id']
           o.relation = t['relation']
           o.form = t['form']
+          o.lemma = t['lemma']
+          o.part_of_speech = t['part_of_speech']
+          o.morphology = t['morphology']
           o.empty_token_sort = t['empty_token_sort']
           o.relation = t['relation']
           o.head_id = t['head_id']
@@ -39,11 +42,11 @@ class AlignedGraphsController < ApplicationController
   def show
     g = AlignedGraph.find_by_sentence_gid(params[:id])
     data = JSON.parse(g.data)
-    sentence1 = data['l']
-    sentence2 = data['r']
-    alignments = data['a']
+    l = data['l']
+    r = data['r']
+    a = data['a']
 
-    graph = GraphAdapter.new(sentence1, sentence2, alignments)
+    graph = GraphAdapter.new(l, r, a)
     image = PROIEL::Visualization::Graphviz.generate('aligned-modern', graph, :svg, direction: params[:direction] || 'TD')
     send_data(image, type: 'image/svg', disposition: 'inline', filename: "#{params[:id]}-aligned.svg")
   end
