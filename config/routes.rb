d33635aa8867d607c751ef3a5393df887b8732a5
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2020-11-10 22:16:56 +0000

    Configure a different base URL in production

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  resources :sentences, only: [:show]
-  resources :graphs, only: [:show]
-  resources :aligned_graphs, only: [:show]
-  resources :sources, only: %i[index show]
-  resources :tokens, only: [:index]
-  resources :dictionaries, only: [:index] do
-    resources :lemmas, only: %i[index show]
+  scope Rails.application.config_for(:base_url)['base_url'] do
+    resources :sentences, only: [:show]
+    resources :graphs, only: [:show]
+    resources :aligned_graphs, only: [:show]
+    resources :sources, only: %i[index show]
+    resources :tokens, only: [:index]
+    resources :dictionaries, only: [:index] do
+      resources :lemmas, only: %i[index show]
+    end
+    resources :chunks, only: [:show]
+    resources :aligned_chunks, only: [:show]
   end
-  resources :chunks, only: [:show]
-  resources :aligned_chunks, only: [:show]
+
   get '/robots.txt' => 'robots_txts#show'
 end
